6538c5add68958c6fa0c562b9a6636739fc4c866
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-07 18:50:24 +0800

    Stack using Array

--- Data Structure - Swift.playground/Pages/Stack (Array).xcplaygroundpage/Contents.swift
+++ Data Structure - Swift.playground/Pages/Stack (Array).xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 class Stack<T> {
     private var elements: [T] = []
+    var isEmpty: Bool {
+        elements.isEmpty
+    }
 
     init() {}
+    
+    func peek() -> T? {
+        elements.last
+    }
+    
+    func push(_ value: T) {
+        elements.append(value)
+    }
+    
+    func pop() -> T? {
+        elements.removeLast()
+    }
 }
